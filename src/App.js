1fa3fe1cc550dac15dc5a1f869148f63e7c19755
MamoshiSE <33344169+MamoshiSE@users.noreply.github.com>
2021-03-15 21:05:35 +0100

    task component with states, delete task events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Lorem ipsum dolor sit amet',
+        day: 'Mar 17th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Morbi sollicitudin ornare nunc',
+        day: 'Mar 19th at 6:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Sit amet condimentum',
+        day: 'Mar 21th at 8:30pm',
+        reminder: false
+    }
+])
+
+const deleteTask = (id) => {
+ setTasks(tasks.filter((task) => task.id !== id))
+}
   return (
     <div className='container'>
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to show'}
     </div>
   );
 }
